53a6baf7f8a9f4c6caabeeb2ab239717a93e9f05
senamelawamly <senamela@wamly.io>
2022-10-13 06:10:00 +0200

    landing page, landing.jpg, logo.png

--- app/src/routes/landing.tsx
+++ app/src/routes/landing.tsx
@@ -10,8 +10,9 @@ import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
 import Link from '@material-ui/core/Link'
 import GitHubIcon from '@material-ui/icons/GitHub'
+import DrawerAppBar from '../components/appBar'
 
-import logoImage from './logo.png'
+import bgImage from '../images/landing.jpg';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,27 +32,35 @@ const Landing: React.FunctionComponent = () => {
     history.push('/signin')
   }
 
+  const signUp = () => {
+    history.push('/signup')
+  }
+
   return (
     <Grid container>
-      <Grid className={classes.root} container direction="column" justify="center" alignItems="center">
+      <Grid className={classes.root} container direction="column" justify="center" alignItems="center" style={{
+          backgroundImage: `url(${bgImage})`,
+        }}>
+       <DrawerAppBar />
         <Box m={2}>
-          <img src={logoImage} width={224} height={224} alt="logo" />
-        </Box>
-        <Box m={2}>
-          <Link underline="none" color="inherit" href="https://github.com/dbroadhurst/aws-cognito-react">
-            <Grid container direction="row" justify="center" alignItems="center">
-              <Box mr={3}>
-                <GitHubIcon fontSize="large" />
-              </Box>
-              <Typography className={classes.title} variant="h3">
-                AWS Cognito Starter
-              </Typography>
-            </Grid>
-          </Link>
-        </Box>
-        <Box m={2}>
-          <Button onClick={signIn} variant="contained" color="primary">
+          <Button onClick={signIn} variant="contained" style={{
+            backgroundColor: '#3d9ae8',
+            color: '#fff',
+            width: '240px',
+            height: '80px',
+            fontSize: '24px'
+          }}>
             SIGN IN
+          </Button>
+          <Button onClick={signUp} variant="contained" style={{
+            backgroundColor: '#6c7cfb',
+            color: '#fff',
+            width: '240px',
+            height: '80px',
+            fontSize: '24px',
+            marginLeft: '20px'
+          }}>
+            Register
           </Button>
         </Box>
       </Grid>
